dbc2d3ee6c1a0cf71a9238a3c10cb70cd2891a60
Uldis Jansons <jansons08@gmail.com>
2022-02-03 12:33:49 +0200

    changed structure

--- app/src/main/java/com/uldisj/vardadienuapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/uldisj/vardadienuapp/view/activities/MainActivity.kt
@@ -1,7 +1,10 @@
-package com.uldisj.vardadienuapp
+package com.uldisj.vardadienuapp.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uldisj.vardadienuapp.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
